6559a3fd355c89b75e44172ca59326964ac27cbf
Mathias Vagni <mathiasvagni@googlemail.com>
2023-05-17 15:17:51 +0200

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,6 @@
 export { PlainClient } from './client';
 
 export {
-  // Input types
-  UpsertCustomerInput,
-  UpsertCustomTimelineEntryInput,
-  CreateIssueInput,
-
-  // Fragments
-  ActorPartsFragment,
-  CustomerActorPartsFragment,
-  CustomerGroupMembershipPartsFragment,
-  DateTimePartsFragment,
-  DeletedCustomerActorPartsFragment,
-  InternalActorPartsFragment,
-  IssuePartsFragment,
-  IssuePriorityPartsFragment,
-  IssueTypePartsFragment,
-  MachineUserActorPartsFragment,
-  MutationErrorPartsFragment,
-  SystemActorPartsFragment,
-  TimelineEntryPartsFragment,
-  UserActorPartsFragment,
-
   // Enums
   ComponentBadgeColor,
   ComponentDividerSpacingSize,
@@ -37,4 +16,27 @@ export {
   MutationFieldErrorType,
   UpsertResult,
   UserStatus,
+} from './graphql/types';
+
+export type {
+  // Input types
+  UpsertCustomerInput,
+  UpsertCustomTimelineEntryInput,
+  CreateIssueInput,
+
+  // Fragment types
+  ActorPartsFragment,
+  CustomerActorPartsFragment,
+  CustomerGroupMembershipPartsFragment,
+  DateTimePartsFragment,
+  DeletedCustomerActorPartsFragment,
+  InternalActorPartsFragment,
+  IssuePartsFragment,
+  IssuePriorityPartsFragment,
+  IssueTypePartsFragment,
+  MachineUserActorPartsFragment,
+  MutationErrorPartsFragment,
+  SystemActorPartsFragment,
+  TimelineEntryPartsFragment,
+  UserActorPartsFragment,
 } from './graphql/types';
