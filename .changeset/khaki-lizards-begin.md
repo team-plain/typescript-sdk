ada8307ec41e2ed7971834ace4a97b088b3df739
Mathias Vagni <mattvagni@users.noreply.github.com>
2023-05-17 15:00:39 +0200

    Export more types (#21)

---
+++ .changeset/khaki-lizards-begin.md
@@ -0,0 +1,5 @@
+---
+'@team-plain/typescript-sdk': minor
+---
+
+Export relevant graphql types to not have to rely on Paramaters in TS as much
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
+import {
+  CustomerGroupMembershipPartsFragment,
+  DateTimePartsFragment,
+  DeletedCustomerActorPartsFragment,
+  InternalActorPartsFragment,
+  IssuePartsFragment,
+  IssuePriorityPartsFragment,
+  IssueTypePartsFragment,
+  MachineUserActorPartsFragment,
+  MutationErrorPartsFragment,
+  SystemActorPartsFragment,
+  TimelineEntryPartsFragment,
+  UserActorPartsFragment,
+} from './graphql/types';
+import { ActorPartsFragment, CustomerActorPartsFragment } from './graphql/types';
+import {
+  UpsertCustomerInput,
+  UpsertCustomTimelineEntryInput,
+  CreateIssueInput,
+} from './graphql/types';
 export { PlainClient } from './client';
+
+export {
+  // Input types
+  UpsertCustomerInput,
+  UpsertCustomTimelineEntryInput,
+  CreateIssueInput,
+
+  // Fragments:
+  ActorPartsFragment,
+  CustomerActorPartsFragment,
+  CustomerGroupMembershipPartsFragment,
+  DateTimePartsFragment,
+  DeletedCustomerActorPartsFragment,
+  InternalActorPartsFragment,
+  IssuePartsFragment,
+  IssuePriorityPartsFragment,
+  IssueTypePartsFragment,
+  MachineUserActorPartsFragment,
+  MutationErrorPartsFragment,
+  SystemActorPartsFragment,
+  TimelineEntryPartsFragment,
+  UserActorPartsFragment,
+};
