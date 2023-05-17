f0d816f42f9033c9bdb9a9f899dbe76c26151a3b
Mathias Vagni <mathiasvagni@googlemail.com>
2023-05-17 15:10:00 +0200

    Clean up exports

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,3 @@
-import { UpsertResult, UserStatus, IssuesSortField } from './graphql/types';
-import {
-  CustomerStatus,
-  CustomersSortField,
-  IssueStatus,
-  MutationErrorType,
-  MutationFieldError,
-  MutationFieldErrorType,
-} from './graphql/types';
-import {
-  CustomerGroupMembershipPartsFragment,
-  DateTimePartsFragment,
-  DeletedCustomerActorPartsFragment,
-  InternalActorPartsFragment,
-  IssuePartsFragment,
-  IssuePriorityPartsFragment,
-  IssueTypePartsFragment,
-  MachineUserActorPartsFragment,
-  MutationErrorPartsFragment,
-  SystemActorPartsFragment,
-  TimelineEntryPartsFragment,
-  UserActorPartsFragment,
-} from './graphql/types';
-import {
-  ActorPartsFragment,
-  CustomerActorPartsFragment,
-  ComponentSpacerSize,
-  ComponentTextSize,
-  ComponentBadgeColor,
-  ComponentDividerSpacingSize,
-  ComponentPlainTextSize,
-  ComponentPlainTextColor,
-  ComponentTextColor,
-} from './graphql/types';
-import {
-  UpsertCustomerInput,
-  UpsertCustomTimelineEntryInput,
-  CreateIssueInput,
-} from './graphql/types';
 export { PlainClient } from './client';
 
 export {
@@ -45,7 +6,7 @@ export {
   UpsertCustomTimelineEntryInput,
   CreateIssueInput,
 
-  // Fragments:
+  // Fragments
   ActorPartsFragment,
   CustomerActorPartsFragment,
   CustomerGroupMembershipPartsFragment,
@@ -76,4 +37,4 @@ export {
   MutationFieldErrorType,
   UpsertResult,
   UserStatus,
-};
+} from './graphql/types';
