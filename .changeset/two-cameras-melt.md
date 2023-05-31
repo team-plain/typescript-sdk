d9de4a12a2f55e1720577fa994e319703a91d265
Mathias Vagni <mathiasvagni@googlemail.com>
2023-05-31 10:07:43 +0200

    Add requestId to errors

---
+++ .changeset/two-cameras-melt.md
@@ -0,0 +1,5 @@
+---
+'@team-plain/typescript-sdk': patch
+---
+
+Added a new 'requestId' field to all errors for better debugging and support when something unexpected happens.
--- src/tests/error-handling.test.ts
+++ src/tests/error-handling.test.ts
@@ -10,7 +10,7 @@ describe('error handling', () => {
   });
 
   test('should return an unknown error when something unexpected is returned', async () => {
-    const scope = interceptor.reply(500, '🌶️');
+    const scope = interceptor.reply(500, '🌶️', { 'apigw-requestid': 'req_4' });
 
     const client = new PlainClient({ apiKey: '123' });
 
@@ -19,13 +19,18 @@ describe('error handling', () => {
     expect(result.error).toEqual({
       type: 'internal_server_error',
       message: 'Internal server error.',
+      requestId: 'req_4',
     });
 
     scope.done();
   });
 
   test('should return a forbidden error when API 401s', async () => {
-    const scope = interceptor.matchHeader('Authorization', 'Bearer 123').reply(401, 'unauthorized');
+    const scope = interceptor
+      .matchHeader('Authorization', 'Bearer 123')
+      .reply(401, 'unauthorized', {
+        'apigw-requestid': 'req_5',
+      });
 
     const client = new PlainClient({ apiKey: '123' });
 
@@ -34,13 +39,16 @@ describe('error handling', () => {
     expect(result.error).toEqual({
       type: 'forbidden',
       message: expect.stringContaining('Authentication failed'),
+      requestId: 'req_5',
     });
 
     scope.done();
   });
 
   test('should return a forbidden error when API 403s', async () => {
-    const scope = interceptor.matchHeader('Authorization', 'Bearer 123').reply(403, 'forbidden');
+    const scope = interceptor.matchHeader('Authorization', 'Bearer 123').reply(403, 'forbidden', {
+      'apigw-requestid': 'req_6',
+    });
 
     const client = new PlainClient({ apiKey: '123' });
 
@@ -49,26 +57,33 @@ describe('error handling', () => {
     expect(result.error).toEqual({
       type: 'forbidden',
       message: expect.stringContaining('Authentication failed'),
+      requestId: 'req_6',
     });
 
     scope.done();
   });
 
   test('should return a forbidden error when API responds with mutation error', async () => {
-    const scope = interceptor.matchHeader('Authorization', 'Bearer 123').reply(200, {
-      data: {
-        updateCustomerGroup: {
-          customerGroup: null,
-          error: {
-            __typename: 'MutationError',
-            message: 'Insufficient permissions, missing "customerGroup:edit".',
-            type: 'FORBIDDEN',
-            code: 'forbidden',
-            fields: [],
+    const scope = interceptor.matchHeader('Authorization', 'Bearer 123').reply(
+      200,
+      {
+        data: {
+          updateCustomerGroup: {
+            customerGroup: null,
+            error: {
+              __typename: 'MutationError',
+              message: 'Insufficient permissions, missing "customerGroup:edit".',
+              type: 'FORBIDDEN',
+              code: 'forbidden',
+              fields: [],
+            },
           },
         },
       },
-    });
+      {
+        'apigw-requestid': 'req_7',
+      }
+    );
 
     const client = new PlainClient({ apiKey: '123' });
 
@@ -77,6 +92,7 @@ describe('error handling', () => {
     expect(result.error).toEqual({
       type: 'forbidden',
       message: 'Insufficient permissions, missing "customerGroup:edit".',
+      requestId: 'req_7',
     });
 
     scope.done();
