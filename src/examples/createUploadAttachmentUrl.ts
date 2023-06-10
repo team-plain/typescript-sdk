eaa7355c08c3d569260e8484a2eaa37bd4d85cd7
Mathias Vagni <mathiasvagni@googlemail.com>
2023-06-10 15:35:54 +0200

    Expose AttachmentType enum

---
+++ src/examples/createUploadAttachmentUrl.ts
@@ -0,0 +1,18 @@
+import { AttachmentType, PlainClient } from '../';
+
+export async function createAttachmentUploadUrl() {
+  const client = new PlainClient({ apiKey: 'XXX' });
+
+  const res = await client.createAttachmentUploadUrl({
+    customerId: 'c_XXXXXXXXXXXXXXXXXXXXXXXXXX',
+    fileName: 'the-filename.jpeg',
+    fileSizeBytes: 32318,
+    attachmentType: AttachmentType.CustomTimelineEntry,
+  });
+
+  if (res.error) {
+    console.error(res.error);
+  } else {
+    console.log(`Attachment upload url created!`, res.data);
+  }
+}
