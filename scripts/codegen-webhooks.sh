e87c1fa627ff7b4c01d920475903e0ed609964ba
Jordan Drake <jordan@plain.com>
2024-03-12 09:44:39 +0000

    Drop commented out script

--- scripts/codegen-webhooks.sh
+++ scripts/codegen-webhooks.sh
@@ -5,5 +5,3 @@
 curl https://core-api.uk.plain.com/webhooks/schema.json -o ./src/webhooks/webhook-schema.json
 
 ./node_modules/.bin/json2ts --input ./src/webhooks/webhook-schema.json --output ./src/webhooks/webhook-schema.ts
-
-# ./node_modules/.bin/json-refs resolve ./src/webhooks/webhook-schema.json | ./node_modules/.bin/json-schema-to-zod | ./node_modules/.bin/prettier --parser typescript > ./src/webhooks/webhook-schema.ts
